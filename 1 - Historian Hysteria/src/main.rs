236be2cd0057b737a48fd68d1eb5364b0165f135
tommyboi <t.austinbennett@gmail.com>
2025-02-01 11:25:18 -0500

    day 1 part 2

--- 1 - Historian Hysteria/src/main.rs
+++ 1 - Historian Hysteria/src/main.rs
@@ -1,9 +1,10 @@
 // https://adventofcode.com/2024/day/1
 
 use std::fs;
+use std::collections::HashMap;
 
 fn main() {
-    //let contents: &str = "3   4\n4   3\n2   5\n1   3\n3   9\n3   3";
+    // let contents: &str = "3   4\n4   3\n2   5\n1   3\n3   9\n3   3";
 
     let contents: String = fs::read_to_string("src/input.txt")
         .expect("Something went wrong reading the file");
@@ -16,15 +17,25 @@ fn main() {
         })
         .collect();
 
-    let mut left: Vec<isize> = pairs.iter().map(|pair| pair.0).collect();
-    let mut right: Vec<isize> = pairs.iter().map(|pair| pair.1).collect();
+    let left: Vec<isize> = pairs.iter().map(|pair| pair.0).collect();
+    let right: Vec<isize> = pairs.iter().map(|pair| pair.1).collect();
 
-    left.sort();
-    right.sort();
+    // left.sort();
+    // right.sort();
 
-    let distance: isize = left.iter().zip(right.iter())
-                                     .map(|(l, r)| (l - r).abs())
-                                     .sum();
+    // count right list by using a HashMap
+    let mut right_count: HashMap<isize, isize> = HashMap::new();
+    for &n in &right {
+        *right_count.entry(n).or_insert(0) += 1;
+    }
 
-    println!("{}", distance);
+    let mut similarity = 0;
+
+    left.iter().for_each(|n| {
+        let count = right_count.get(n).unwrap_or(&0);
+        // println!("{}: {} = score: {}", n, count, n * count);
+        similarity += n * count;
+    });
+
+    println!("Similarity: {}", similarity);
 }
