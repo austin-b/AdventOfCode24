5e8a4d014b91911fa6ab8d3ebcde0a216930be0b
tommyboi <t.austinbennett@gmail.com>
2025-02-04 10:09:43 -0500

    day 2 part 2

--- 2 - Red-Nosed Reports/src/main.rs
+++ 2 - Red-Nosed Reports/src/main.rs
@@ -15,20 +15,49 @@ fn main() {
         })
         .collect();
 
-    let safe: Vec<bool> = reports.iter().map(|report| {
-        // determine if the report is sorted in ascending or descending order
-        let s: bool = report.is_sorted() || report.is_sorted_by(|a, b| b <= a);
-
-        // if so, ensure that the difference between each number is between 1 and 3
-        if s { report.is_sorted_by(|a, b| {
-            let diff = (a - b).abs();
-            diff >=1 && diff <= 3
-        })}
-        else { false }
-        }).collect();
+    let safe: Vec<bool> = reports.iter().map(|report| safety_checker(report)).collect();
 
     println!("{:?}", safe);
 
     let safe_count = safe.iter().filter(|&s| *s).count();
     println!("Safe reports: {}", safe_count);
+}
+
+fn safety_checker(report: &Vec<isize>) -> bool {
+    let mut safe: bool;
+
+    // determine if the report is sorted in ascending or descending order
+    safe = report.is_sorted() || report.is_sorted_by(|a, b| b <= a);
+
+    // if so, ensure that the difference between each number is between 1 and 3
+    if safe { 
+        safe = report.is_sorted_by(|a, b| {
+            let diff = (a - b).abs();
+            diff >=1 && diff <= 3
+        });
+    }
+
+    // if the report is not safe, remove one number from the report at a time and check again
+    if !safe {
+        for i in 0..report.len() {
+            let mut new_report = report.clone();
+            new_report.remove(i);   // remove one number from the report
+
+            // determine if the report is sorted in ascending or descending order
+            safe = new_report.is_sorted() || new_report.is_sorted_by(|a, b| b <= a);
+
+            // if so, ensure that the difference between each number is between 1 and 3
+            if safe { 
+                safe = new_report.is_sorted_by(|a, b| {
+                    let diff = (a - b).abs();
+                    diff >=1 && diff <= 3
+                });
+            }
+
+            // if the report is safe, break out of the loop
+            if safe { break; }
+        }
+    } 
+
+    safe
 }
